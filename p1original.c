f79e9310b72970d3739fbd979dfdd2af846e53dd
Mahesh <94962227+Mahimahesh27@users.noreply.github.com>
2022-03-08 04:32:16 +0000

    done

--- p1original.c
+++ p1original.c
@@ -1,40 +1,66 @@
 #include <stdio.h>
 
-int gcd(int a, int b)
 
+void input(int *n1, int *den1, int *n2, int *den2);
+
+int gcd(int a, int b);
+
+void smallest(int den3, int n3);
+
+void add_frac(int n1, int den1, int n2, int den2, int *n3, int *den3);
+
+void output(int n1, int den1, int n2, int den2, int n3, int den3);
+
+int main()
+{
+  int *n1, *den1, *n2, *den2, *n3, *den3;
+  input(&n1, &den1, &n2, &den2);
+  add_frac(n1, den1, n2, den2, &n3, &den3);
+  output(n1, den1, n2, den2, n3, den3); 
+}
+
+void input(int *n1, int *den1, int *n2, int *den2)
+{
+  printf("Enter Numerator 1: ");
+  scanf("%d", n1);
+
+  printf("Enter Denominator 1: ");
+  scanf("%d", den1);
+
+  printf("Enter Numerator 2: ");
+  scanf("%d", n2);
+
+  printf("Enter Denominator 2: ");
+  scanf("%d", den2);
+}
+
+int gcd(int a, int b) 
 {
    if (a == 0)
       return b;
-   return gcd(b%a, a);
+   return gcd( b%a , a);
 }
 
-void smallest(int &den3, int &n3)
-
+void smallest(int den3, int n3) 
 {
    // Finding gcd of both terms
    int common_factor = gcd(n3,den3);
-   den3 = den3/common_factor;
-   n3 = n3/common_factor;
+   den3 = den3 / common_factor;
+   n3 = n3 / common_factor;
 }
 
-void add_frac(int n1, int den1, int n2, int den2, int &n3, int &den3)
-
+void add_frac(int n1, int den1, int n2, int den2, int *n3, int *den3) 
 {
-   // to find the gcd of den1 and den2
-   den3 = gcd(den1,den2);
-    // LCM * GCD = a * b
-   den3 = (den1*den2) / den3;
-   // Changing the inputs to have same denominator
-   // Numerator of the final fraction obtained
-   n3 = (n1)*(den3/den1) + (n2)*(den3/den2);
-   smallest(den3,n3);
+   
+   *den3 = gcd(den1,den2);
+   
+   *den3 = (den1*den2) / (*den3);
+   
+   *n3 = (n1)*( *den3 / den1) + (n2)*((*den3)/den2);
+   smallest(*den3, *n3);
 }
 
-int main() 
-
+void output(int n1, int den1, int n2, int den2, int n3, int den3)
 {
-   int n1=1, den1=4, n2=2, den2=12, den3, n3;
-   add_frac(n1, den1, n2, den2, n3, den3);
-   printf("%d/%d + %d/%d = %d/%d\n", n1, den1, n2, den2, n3, den3);
-   return 0;
+  printf("The summ of 2 fractions %d/%d and %d/%d= %d/%d", n1, den1, n2, den2, n3, den3);
 }
